e2b00e84957b8ada0086232e9b1a205eb5763010
odinokun <odinokun@gmail.com>
2019-08-04 22:23:38 +0300

    did base settings

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,13 +2,13 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/svg4everybody/dist/svg4everybody.min.js'
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
 
   // include jQuery UI
   // './src/vendors/jquery-ui/jquery-ui.min.js',
 
   // include Slick slider
-  // './node_modules/slick-carousel/slick/slick.min.js',
+  './node_modules/slick-carousel/slick/slick.min.js'
 
   // include fancybox3
   // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
